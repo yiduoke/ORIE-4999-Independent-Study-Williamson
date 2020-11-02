31c24d2814f5750961c7bd589bef4916bee37de6
Yiduo Ke <yk467@cornell.edu>
2020-11-02 00:49:04 -0500

    flip a coin randomized approximation max cut doneeee

--- code/brute_force_max_cut.jl
+++ code/brute_force_max_cut.jl
@@ -2,6 +2,18 @@ import Pkg
 Pkg.add("Combinatorics")
 using Combinatorics
 
+"""
+  brute_force_max_cut(adj_lists) 
+
+returns, for a given graph in adjacency list form `adj_lists`,
+the max cut value and its corresponding vertex set partition.
+
+`adj_lists` is a Dict with each vertex as a key, whose value 
+is its neighboring vertices in the form of an Array. 
+
+The algorithm is brute force and thus exponential time in 
+the number of vertices.
+"""
 function brute_force_max_cut(adj_lists)
   n = length(adj_lists)
   all_possible_A = vcat([collect(combinations(1:n,i)) for i=1:n-1]...) # all non-empty proper subsets of V
@@ -9,19 +21,23 @@ function brute_force_max_cut(adj_lists)
   current_max_cut = 0 # running max cut value
 
   for A in all_possible_A # iterationg through each possible partition
-    running_B = reduce(vcat, [setdiff(adj_lists[vertex], A) for vertex in A]) # cardinality of running_B is the value of (A,V-A) cut
+    current_cut_value = length(reduce(vcat, [setdiff(adj_lists[vertex], A) for vertex in A])) # value of (A,V-A) cut
 
-    if length(running_B) > current_max_cut # updating the values when we get a new max
+    if current_cut_value > current_max_cut # updating the values when we get a new max
       current_max_partition = A
-      current_max_cut = length(running_B)
+      current_max_cut = current_cut_value
     end
   end
 
-  println("max cut: $current_max_partition")
-  println("max cut value: $current_max_cut\n")
+  # println("max cut: $current_max_partition")
+  # println("max cut value: $current_max_cut\n")
   return (current_max_cut, current_max_partition)
 end
 
-brute_force_max_cut(Dict(1=>[2,4], 2=>[1,3], 3=>[2,4], 4=>[1,3])) # max cut should be 4
-brute_force_max_cut(Dict(1=>[2,5], 2=>[1,3,5], 3=>[2,4], 4=>[3,5], 5=>[1,2,4])) # max cut should be 5
-brute_force_max_cut(Dict(1=>[2,3], 2=>[1,4,5], 3=>[1,6,7])) # this is a perfect binary tree. max cut should be 6
+test_case_1 = brute_force_max_cut(Dict(1=>[2,4], 2=>[1,3], 3=>[2,4], 4=>[1,3]))
+test_case_2 = brute_force_max_cut(Dict(1=>[2,5], 2=>[1,3,5], 3=>[2,4], 4=>[3,5], 5=>[1,2,4]))
+test_case_3 = brute_force_max_cut(Dict(1=>[2,3], 2=>[1,4,5], 3=>[1,6,7]))
+
+println("test 1. max cut: $(test_case_1[1]) (should be 4). max cut partition: $(test_case_1[2])")
+println("test 2. max cut: $(test_case_2[1]) (should be 5). max cut partition: $(test_case_2[2])")
+println("test 3. max cut: $(test_case_3[1]) (should be 6). max cut partition: $(test_case_3[2])")
---
+++ code/flip_coin_max_cut.jl
@@ -0,0 +1,34 @@
+import Pkg
+Pkg.add("Combinatorics")
+using Combinatorics
+using Statistics
+
+
+"""
+    flip_coin_max_cut(adj_lists) 
+
+returns, for a given graph in adjacency list form `adj_lists`,
+the 2-approximation max cut value and its corresponding vertex 
+set partition. 
+
+`adj_lists` is a Dict with each vertex as a key, whose value 
+is its neighboring vertices in the form of an Array. 
+
+This is a randomized algorithm and linear in the number
+of vertices.
+"""
+function flip_coin_max_cut(adj_lists)
+    A = reduce(vcat,[rand(0:1)==0 ? [i] : [] for i in 1:length(adj_lists)])
+    cut_value = length(A)>0 ? length(reduce(vcat, [setdiff(adj_lists[vertex], A) for vertex in A])) : 0
+
+    return (cut_value, A)
+end
+
+
+test_case_1 = mean([flip_coin_max_cut(Dict(1=>[2,4], 2=>[1,3], 3=>[2,4], 4=>[1,3]))[1] for i in 1:50])
+test_case_2 = mean([flip_coin_max_cut(Dict(1=>[2,5], 2=>[1,3,5], 3=>[2,4], 4=>[3,5], 5=>[1,2,4]))[1] for i in 1:50])
+test_case_3 = mean([flip_coin_max_cut(Dict(1=>[2,3], 2=>[1,4,5], 3=>[1,6,7]))[1] for i in 1:50])
+
+println("test 1. mean randomized max cut: $(test_case_1) (should be >=2)")
+println("test 2. mean randomized max cut: $(test_case_2) (should be >=2.5)")
+println("test 3. mean randomized max cut: $(test_case_3) (should be >=3)")
