90d5a1094d83722b935b60e43cbd8cb6f7d05d94
Yiduo Ke <yk467@cornell.edu>
2020-11-11 06:28:23 -0500

    FINALLY SOMETHING THAT ISN'T UNFEASABLE PRAISE JESUS

--- code/SDP_max_cut.jl
+++ code/SDP_max_cut.jl
@@ -1,9 +1,9 @@
 import Pkg
 Pkg.add("Combinatorics")
 Pkg.add("JuMP")
-Pkg.add("Ipopt")
+Pkg.add("ProxSDP")
 using Combinatorics
-using JuMP, Ipopt
+using JuMP, ProxSDP, LinearAlgebra
 
 function unique_edges(adj_lists)
     n = length(adj_lists)
@@ -14,7 +14,6 @@ end
 function adj_lists_to_matrix(adj_lists)
     n = length(adj_lists)
     adj_matrix = zeros(n,n)
-    # [adj_matrix[i,j] = 1 for i ∈ 1:n, j ∈ adj_lists[i]]
     [adj_matrix[i,j] = 1 for i ∈ 1:n for j ∈ adj_lists[i]]
     return adj_matrix
 end
@@ -22,14 +21,15 @@ end
 function SDP_force_max_cut(adj_lists)
     n = length(adj_lists)
     adj_matrix = adj_lists_to_matrix(adj_lists)
-    model = Model(with_optimizer(Ipopt.Optimizer))
-    @variable(model,  V[1:n, 1:n])
-    [@constraint(model, V[1:n, i]'V[1:n, i] == 1) for i ∈ 1:n]
-    @objective(model, Max, 0.5 * adj_matrix - V'V)
+    model = Model(with_optimizer(ProxSDP.Optimizer))
+    @variable(model,  V[1:n, 1:n], PSD)
+    @objective(model, Max, 0.5 * dot(adj_matrix, V))
+    @constraint(model, diag(V) .== 1)
     JuMP.optimize!(model)
 
-    return JuMP.value.(y)
+    println(JuMP.value.(V))
+    return JuMP.value.(V)
 end
 
-graph_1 = Dict(1=>[2,4], 2=>[1,3], 3=>[2,4], 4=>[1,3])
-println(SDP_force_max_cut(graph_1))
+graph_1 = Dict(1=>[2,3], 2=>[1,3], 3=>[1,2])
+println(size(SDP_force_max_cut(graph_1)))
