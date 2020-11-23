252ba6632e051904fa4f4e00c12d46d01b1f5358
Yiduo Ke <yk467@cornell.edu>
2020-11-23 03:50:48 -0500

    two_thresholds_spectral_cut done i think but BIG question marks bc denominator could be 0

--- code/trevisan_max_cut.jl
+++ code/trevisan_max_cut.jl
@@ -6,6 +6,27 @@ include("helpers.jl")
 
 
 """
+    y_ik(x)
+
+given a vector `x` of length n, and indices i and k, 
+
+cases:
+    x[i] < -abs(x[k]): return -1
+    x[i] > abs(x[k]): return 1
+    abs(x[i]) <= abs(x[k]): return 0
+
+"""
+function y_ik(x,i,k)
+    if x[i] < -abs(x[k])
+        -1
+    elseif x[i] > abs(x[k])
+        1
+    elseif abs(x[i]) <= abs(x[k])
+        0
+    end
+end
+
+"""
     two_thresholds_spectral_cut(adj_lists)
 
 given a graph `adj_lists`, returns the vector specified in the
@@ -19,9 +40,16 @@ function two_thresholds_spectral_cut(adj_lists)
     A = adj_lists_to_matrix(adj_lists)
     n = length(adj_lists)
 
-    sqrt_D = zeros(n,n)
-    [sqrt_D[i,i] = D[i,i]^(-1/2) for i ∈ 1:n]
-    x = smallest_eigenvector(sqrt_D * A * sqrt_D)
+    neg_sqrt_D = zeros(n,n)
+    [neg_sqrt_D[i,i] = D[i,i]^(-1/2) for i ∈ 1:n]
+    x = smallest_eigenvector(neg_sqrt_D * A * neg_sqrt_D)
+
+    Y = zeros(n,n)
+    [Y[i,k] = y_ik(x,i,k) for i ∈ 1:n for k ∈ 1:n]
+
+    ratios = zeros(n)
+    ratios = [(sum(A[i,j] * abs(Y[i,k] + Y[j,k]) for i ∈ 1:n for j ∈ 1:n) / sum(D[i,i] * abs(Y[i,k]) for i ∈ 1:n)) for k ∈ 1:n]
+    return Y[:, findmin(ratios)[2]]
 end
 
 """
