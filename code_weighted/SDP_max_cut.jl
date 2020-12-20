ff3af21bf2d3279c0665600ccfa5f14252c2cdd0
Yiduo Ke <yk467@cornell.edu>
2020-12-20 01:41:27 -0500

    SDP done converting to weighted version

--- code_weighted/SDP_max_cut.jl
+++ code_weighted/SDP_max_cut.jl
@@ -10,22 +10,18 @@ include("helpers.jl")
 
 
 """
-    SDP_max_cut(adj_lists) 
+    SDP_max_cut(adj_matrix) 
 
-returns, for a given graph in adjacency list form `adj_lists`,
+returns, for a given graph in adjacency matrix form `adj_matrix`,
 the 0.878-approximation max cut value and its corresponding vertex 
 set partition. The expected cut value is 0.878*OPT, where OPT is 
 the actual max cut.
 
-`adj_lists` is a Dict with each vertex as a key, whose value 
-is its neighboring vertices in the form of an Array. 
-
 This is a randomized approximation algorithm and runs in time 
 polynomial to the number of vertices.
 """
-function SDP_max_cut(adj_lists)
-    n = length(adj_lists)
-    adj_matrix = adj_lists_to_matrix(adj_lists)
+function SDP_max_cut(adj_matrix)
+    n = num_vertices(adj_matrix)
     model = Model(with_optimizer(ProxSDP.Optimizer))
     @variable(model, V[1:n, 1:n], PSD)
     @objective(model, Max, 0.25 * dot(adj_matrix, 1 .- V))
@@ -41,6 +37,9 @@ function SDP_max_cut(adj_lists)
     
     Z = factorization.U
     A = filter(i -> dot(Z[1:n, i], r) < 0, 1:n)
-    cut_val = cut_value(adj_lists, A)
+    cut_val = cut_value(adj_matrix, A)
     return cut_val, A, maximized_objective
 end
+
+adj_matrix = [0 2 0 3; 2 0 4 0; 0 4 0 2; 3 0 2 0]
+println(SDP_max_cut(adj_matrix))
